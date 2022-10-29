6ae732da6c492da0b23b9c5b9018b84ece3b7b21
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-29 12:59:59 +0900

    x

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,40 +1,35 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler')
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 
-const router = express.Router();
-
 const db = require("../../db/models/")
 
+const router = express.Router();
+
 router.get('/register', (req, res) => {
+
     res.render('users/register');
 });
 
-router.post('/register', async (req, res, next) => {
-    try {
-        console.log("hit post register")
-        console.log(req.body)
-        const foundUserWithEmail = await db.User.findOne({ where: { email: req.body.email } });
+router.post('/register', asyncHandler(async (req, res, next) => {
+    const foundUserWithEmail = await db.User.findOne({ where: { email: req.body.email } });
 
-        if (foundUserWithEmail) {
-            res.statusCode = 404
-            throw new Error('user already exists');
-        }
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword
-        const user = await db.User.create(req.body)
-        try {
-            req.session.passport = { user: user.id }
-        } catch (error) {
-            console.log({ error })
-        }
-
-        res.redirect('/campgrounds');
-    } catch (e) {
-        // req.flash('error', e.message);
-        res.redirect('/register');
+    if (foundUserWithEmail) {
+        res.statusCode = 404
+        throw new Error('user already exists');
     }
-});
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword
+    const user = await db.User.create(req.body)
+    try {
+        req.session.passport = { user: user.id }
+    } catch (error) {
+        console.log({ error })
+    }
+
+    res.redirect('/campgrounds');
+}))
 
 router.get('/login', (req, res) => {
 
@@ -51,6 +46,7 @@ router.post('/login', passport.authenticate('local',
 
 router.get('/logout', (req, res) => {
     req.session = null;
+
     res.redirect('/campgrounds');
 });
 
