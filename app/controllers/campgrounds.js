45905235b9919720deaac587b3f5f1d7e827e69e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-06 20:01:09 +0900

    s 評価平均計算

--- app/controllers/campgrounds.js
+++ app/controllers/campgrounds.js
@@ -1,3 +1,4 @@
+const { QueryTypes } = require('sequelize');
 const asyncHandler = require('express-async-handler')
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapboxToken = process.env.MAPBOX_TOKEN;
@@ -60,6 +61,21 @@ module.exports.createCampground = async (req, res) => {
 
 // CRUD Read
 module.exports.index = async (req, res) => {
+    // 評価平均計算 バッチ化
+    const aveRates = await sequelize.query('SELECT campground_id, AVG(rating) as aveRate FROM reviews GROUP BY campground_id;', {
+        type: QueryTypes.SELECT
+    });
+    // console.log("JSON.stringify(aveRate, null, 2)", JSON.stringify(aveRates, null, 2))
+
+    aveRates.forEach(async (aveRate) => {
+        await Campground.update(
+            { average_rating: aveRate.aveRate }, {
+            where: {
+                id: aveRate.campground_id
+            }
+        });
+    })
+
     const campgrounds = await Campground.findAll({
         include: [
             {
@@ -82,7 +98,7 @@ module.exports.index = async (req, res) => {
         )
     })
 
-    console.log("campgrounds", JSON.stringify(campgrounds, null, 2))
+    // console.log("campgrounds", JSON.stringify(campgrounds, null, 2))
 
     res.render('campgrounds/index', { campgrounds, campgroundMap });
 }
@@ -111,7 +127,7 @@ module.exports.showCampground = async (req, res) => {
             }
         ]
     });
-    console.log("JSON.stringify(campground, null, 2)", JSON.stringify(campground, null, 2))
+    // console.log("JSON.stringify(campground, null, 2)", JSON.stringify(campground, null, 2))
 
     const geometry = JSON.parse(campground.dataValues.geometry)
     const coordinates = geometry.coordinates
